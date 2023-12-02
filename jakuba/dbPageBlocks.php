cdeeb15ad7e41c2e28ae07dd6818b752ea2d3cd9
ftp <lexium_ja@artek.beget.ru>
2023-12-02 17:13:28 +0300

    Show blocks by url

--- jakuba/dbPageBlocks.php
+++ jakuba/dbPageBlocks.php
@@ -1,4 +1,6 @@
 <?php
+
+
 require_once( $_SERVER['DOCUMENT_ROOT'] . "/" . DIR_CORE . "/db.php");
 
 class DBPageBlocks extends DB {
@@ -7,12 +9,21 @@ class DBPageBlocks extends DB {
         return $this->select("Pages", ["id"], ["url" => $url])[0]["id"];
     }
 
-    /*
-    *  
-    * return array из id блоков
-    */
-    public function getPageBlocksByUrl(string $url) {
+    public function getPageBlocksIdByUrl(string $url) {
         return explode(",", $this->select("Pages", ["blocks"], ["url" => $url])[0]["blocks"]);
+    }
+
+    public function getPageBlocksByUrl(string $url) {
+        $blocksId = explode(",", $this->select("Pages", ["blocks"], ["url" => $url])[0]["blocks"]);
+
+        $placeholders = str_repeat('?,', count($blocksId) - 1) . '?';
+
+        $sql = "SELECT b.id, b.content, tob.path FROM TypesOfBlocks tob JOIN Blocks b ON b.type = tob.id WHERE b.id IN ($placeholders)";    
+        $pdo = $this->getPDO();
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($blocksId);  
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
 }
--- jakuba/index.php
+++ jakuba/index.php
@@ -1,16 +1,16 @@
 <?php 
 
-//require_once( $_SERVER['DOCUMENT_ROOT'] . "/config.php");
-
-
 $page = new DBPageBlocks(DB_NAME, DB_USER, DB_PASSWORD, DB_HOST);
 
 $url = prepare_url($_SERVER['REQUEST_URI']);
 
-$pageId = $page->getPageBlocksByUrl($url);
+$arrPage = $page->getPageBlocksByUrl($url);
 
-print_r($pageId);
-
+foreach ($arrPage as $blockProperty) {
+    //echo "ID: " . $item['id'] . ", Content: " . $item['content'] . ", Path: " . $item['path'] . "<br>";
+    
+    include $_SERVER['DOCUMENT_ROOT'] . "/" . DIR_MODULES . "/" . "blocks" . "/" . $blockProperty['path'] . "/view/index.php";
+}
 
 function prepare_url($url) { // человекопонятный url
     $arr_url = array_filter(explode("/", $url), 'strlen');
