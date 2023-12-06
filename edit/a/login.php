ee80d571f74e0415bee3e761e1cb4fdbb0af805f
OlgaRhythm <y.o.a@yandex.ru>
2023-12-06 22:23:05 +0300

    Styles for login screen added

--- edit/a/login.php
+++ edit/a/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once($_SERVER['DOCUMENT_ROOT'] . "/" ."config.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/" . "config.php");
 $title = "Вход Admin";
 //include $_SERVER['DOCUMENT_ROOT'] . "/" . DIR_ADMIN . "/template/header.php";
 
@@ -19,18 +19,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $error = "Неверное имя пользователя или пароль";
     }
 }
-
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" type="text/css" href="/<?= DIR_ADMIN ?>/<?= DIR_ADMIN_TEMPLATE ?>/css/style.css">
+</head>
+
 <body>
-    <form method="post" action="">
-        <label for="username">Имя пользователя:</label><br>
-        <input type="text" id="username" name="username"><br>
-        <label for="password">Пароль:</label><br>
-        <input type="password" id="password" name="password"><br><br>
-        <input type="submit" value="Войти">
-    </form>
-    <?php if(isset($error)) { echo $error; } ?>
+    <div class="login_screen_wrapper">
+        <form class="login_screen" method="post" action="">
+            <h2>Вход в Jakuba CMS Admin</h2>
+            <label for="username">Имя пользователя:</label><br>
+            <input type="text" id="username" name="username"><br>
+            <label for="password">Пароль:</label><br>
+            <input type="password" id="password" name="password"><br><br>
+            <input type="submit" value="Войти">
+        </form>
+        <?php if (isset($error)) {
+            echo "<p>" . $error . "</p>";
+        } ?>
+    </div>
+
 </body>
+
 </html>
--- edit/verticalMenu.php
+++ edit/verticalMenu.php
@@ -2,4 +2,5 @@
     <li><a href="/edit/editPage.php">Страницы</a></li>
     <li><a href="/edit/blocks.php">Типы блоков</a></li>
     <li><a href="/edit/metaBlocks.php">Мета блоки</a></li>
+    <li style="float:right;margin-right:30px;"><a href="/edit/a/logout.php">Выйти</a></li>
 </ul>
