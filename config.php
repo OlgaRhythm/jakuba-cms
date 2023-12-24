42ed646e10d73a5ab25f4b97632c0a0c796be99b
OlgaRhythm <87225985+OlgaRhythm@users.noreply.github.com>
2023-12-24 18:22:08 +0300

    config file added

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+    define("DB_NAME", "");
+    define("DB_USER", "");
+    define("DB_PASSWORD", "");
+    define("DB_HOST", "");
+
+    define("DIR_CORE", "jakuba");
+    define("DIR_ADMIN", "edit");
+    define("DIR_ADMIN_TEMPLATE", "template");
+    define("DIR_MEDIA", "media");
+    define("DIR_MODULES", "modules");
+    define("DIR_TEMPLATES", "templates");
+    define("DIR_MAINTEMPLATE", "main");
